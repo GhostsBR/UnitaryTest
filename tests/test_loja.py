60bd1331f0bf806a2ec3c2c53840825a24e36266
Gustavo <gustavolemos7@hotmail.com>
2021-04-22 17:16:37 -0300

    Feat: Finished unitary loja test

--- tests/test_loja.py
+++ tests/test_loja.py
@@ -1,18 +1,21 @@
 from unittest import TestCase, mock
+from funcoes.loja import Loja
 
 
 class TestLoja(TestCase):
-    from funcoes import loja
-    test_loja = loja.Loja()
+    @mock.patch("funcoes.banco.open", mock.mock_open(read_data="1234;3322;10/24;165;500;799.0"))
+    def test_request_payment_with_card_is_working(self):
+        test_card_data = dict(numero="1234", senha="3322")
+        self.assertIsNone(Loja().solicitar_pagamento_com_cartao(test_card_data, 10.0, "0"))
+        test_card_data_fail = dict(numero="134", senha="3322")
+        self.assertIsNone(Loja().solicitar_pagamento_com_cartao(test_card_data_fail, 10.0, "0"))
 
-    # @mock.patch("funcoes.banco.open", mock.mock_open(read_data="1234;3322;10/24;165;500;799.0"))
-    # def test_request_payment_card_is_working(self):
-    #     test_card_data_valid = dict(numero="1234", senha="3322")
-    #     self.assertEqual( , self.test_loja.solicitar_pagamento_com_cartao(test_card_data_valid, 10.0, "0"))
-
-    def test_request_boleto_is_working(self):
+    @mock.patch("funcoes.banco.Banco.gerar_boleto")
+    def test_request_boleto_is_working(self, mock_banco):
+        mock_banco.return_value = dict(conta_recebedora='000124578', código='001', data_validade=130, valor=10.0)
+        test_return_value = dict(conta_recebedora='000124578', código='001', data_validade=130, valor=10.0)
         test_purchase_valid_data = dict(valor=10.0, data=127)
-        self.assertEqual(dict(valor=10.0, data=127), test_purchase_valid_data)
+        self.assertEqual(test_return_value, Loja().solicitar_boleto(test_purchase_valid_data))
 
     def test_payment_with_money_is_working(self):
-        self.assertIsNone(self.test_loja.efetuar_pagamento_com_dinheiro(8.0, 10.0))
+        self.assertIsNone(Loja().efetuar_pagamento_com_dinheiro(8.0, 10.0))
